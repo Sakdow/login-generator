94c5b9b563f64698f78653bdfb531ed09a148a67
Sakdow <nathan.roche31@gmail.com>
2018-11-21 11:36:57 +0100

    close #8

--- src/main/java/geco/LoginGenerator.java
+++ src/main/java/geco/LoginGenerator.java
@@ -36,7 +36,11 @@ public class LoginGenerator {
      */
     public String generateLoginForNomAndPrenom(String nom, String prenom) {
         String p = deAccent(prenom.substring(0,1).toUpperCase());
-        String n = deAccent(nom.substring(0,3).toUpperCase());
+        int nbLetter = 3;
+        if (nom.length() < nbLetter) {
+            nbLetter = nom.length();
+        }
+        String n = deAccent(nom.substring(0,nbLetter).toUpperCase());
         String login = p+n ;
         if (loginService.loginExists(login)) {
             List<String> allLoginsStartingWith =
