0f6f50503d4f640a0cfc685bfe32f65bede25a0a
Sakdow <nathan.roche31@gmail.com>
2018-11-21 11:15:28 +0100

    v1.1.0-RELEASE Add class PasswordGeneration and its test class PasswordGenerationTest

--- src/test/java/geco/PasswordGenerationTest.java
+++ src/test/java/geco/PasswordGenerationTest.java
@@ -1,5 +1,24 @@
+package geco;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PasswordGenerationTest {
 
+    private static PasswordGeneration p;
+
+
+    @BeforeClass
+    public static void setUp(){
+        p = new PasswordGeneration();
+    }
+
+
+    @Test
+    public void getRandomPassword() {
+        String password = p.getRandomPassword();
+        assertEquals(8, password.length());
+    }
 }
